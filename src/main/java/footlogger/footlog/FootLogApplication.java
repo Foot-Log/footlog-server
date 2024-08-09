e7dfa7e02400a4c2825b3d69dc3da0c5fe8c080e
이예지 <oyatpeach@naver.com>
2024-08-09 09:55:20 +0900

    chore : 프로젝트 초기 설정

---
+++ src/main/java/footlogger/footlog/FootLogApplication.java
@@ -0,0 +1,13 @@
+package footlogger.footlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/footlogger/footlog/FootLogApplicationTests.java
@@ -0,0 +1,13 @@
+package footlogger.footlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
