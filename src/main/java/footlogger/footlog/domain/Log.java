d83fbd208549a13889822f9634766ed1f17efa85
인태영 <intaeyoung@intaeyeong-ui-noteubug.local>
2024-08-27 16:41:36 +0900

    [Feat] 도메인 생성

---
+++ src/main/java/footlogger/footlog/domain/Log.java
@@ -0,0 +1,30 @@
+package footlogger.footlog.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Log {
+    @Id
+    @GeneratedValue
+    @Column(name = "log_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String logContent;
+    private String photos;
+}
---
+++ src/main/java/footlogger/footlog/domain/SaveCourse.java
@@ -0,0 +1,27 @@
+package footlogger.footlog.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaveCourse {
+    @Id
+    @GeneratedValue
+    @Column(name = "savecourse_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
