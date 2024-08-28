b914a64a679cb0250f90461d252ae0b410d6dd8b
이예지 <oyatpeach@naver.com>
2024-08-28 12:42:59 +0900

    fix : jwt error 해결 #3

--- src/main/java/footlogger/footlog/domain/jwt/JWTProperties.java
+++ src/main/java/footlogger/footlog/domain/jwt/JWTProperties.java
@@ -1,12 +1,14 @@
 package footlogger.footlog.domain.jwt;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "jwt")
 @Getter
+@Setter
 public class JWTProperties {
     private String secret;
     private Long accessTokenValidity;
