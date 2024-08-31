ecfbf67ccc4d7188e966e0fe2531ec1c7ffd4c35
인태영 <intaeyoung@intaeyeong-ui-noteubug.local>
2024-08-31 17:57:01 +0900

    [Feat] CourseResponseDTO 추가

---
+++ src/main/java/footlogger/footlog/web/dto/response/CourseResponseDTO.java
@@ -0,0 +1,16 @@
+package footlogger.footlog.web.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class CourseResponseDTO {
+    private Long course_id;
+    private List<String> image;
+    private String area;
+    private String name;
+    private Boolean isSave;
+}
