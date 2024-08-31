aa82fba434b3eb407d45d0db38f090646497dd49
이예지 <oyatpeach@naver.com>
2024-08-31 21:05:24 +0900

    Feat : 유저 정보 반환 dto 변경 #3

--- src/main/java/footlogger/footlog/web/controller/UserController.java
+++ src/main/java/footlogger/footlog/web/controller/UserController.java
@@ -33,10 +33,11 @@ public class UserController {
             KakaoUserInfoResponseDto userInfo = kakaoService.getUserInfo(kakaoTokenResponseDto.getAccessToken());
             kakaoService.handleUserRegistration(userInfo, kakaoTokenResponseDto);
 
-            return new ResponseEntity<>(ApiResponse.onSuccess(Map.of(
-                    "accessToken", kakaoTokenResponseDto.getAccessToken(),
-                    "refreshToken", kakaoTokenResponseDto.getAccessToken()
-            )), HttpStatus.OK);
+            UserResponseDto.LoginResultDto loginResultDto = UserResponseDto.LoginResultDto.builder()
+                    .accessToken(kakaoTokenResponseDto.getAccessToken())
+                    .refreshToken(kakaoTokenResponseDto.getAccessToken())
+                    .build();
+            return new ResponseEntity<>(ApiResponse.onSuccess(loginResultDto), HttpStatus.OK);
         } catch (Exception e) {
             log.error("Error during Kakao callback: ", e);
             return new ResponseEntity<>(ApiResponse.onFailure(_INTERNAL_SERVER_ERROR, null), HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/footlogger/footlog/web/dto/response/KakaoTokenResponseDto.java
+++ src/main/java/footlogger/footlog/web/dto/response/KakaoTokenResponseDto.java
@@ -2,6 +2,8 @@ package footlogger.footlog.web.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,4 +25,5 @@ public class KakaoTokenResponseDto {
     public Integer refreshTokenExpiresIn;
     @JsonProperty("scope")
     public String scope;
+
 }
