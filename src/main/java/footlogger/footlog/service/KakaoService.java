0b2fdcc3d4ef16024e369dfb22e913f89c4f1e64
이예지 <oyatpeach@naver.com>
2024-08-31 10:58:05 +0900

    Feat : accessToken받는 코드 수정 #3

--- src/main/java/footlogger/footlog/service/KakaoService.java
+++ src/main/java/footlogger/footlog/service/KakaoService.java
@@ -1,112 +1,71 @@
 package footlogger.footlog.service;
 
+import ch.qos.logback.core.joran.util.beans.BeanDescriptionFactory;
 import footlogger.footlog.domain.User;
 import footlogger.footlog.domain.jwt.JWTUtil;
+import footlogger.footlog.web.dto.response.KakaoTokenResponseDto;
+import io.netty.handler.codec.http.HttpHeaderValues;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
 import java.util.Map;
 import java.util.Optional;
 
+@Slf4j
 @Service
-@RequiredArgsConstructor
+
 public class KakaoService {
 
-    @Value("${kakao.client-id}")
-    private String clientId;
+    private final String clientId;
+    private final String KAUTH_TOKEN_URL_HOST = "https://kauth.kakao.com";
+    private final String KAUTH_USER_URL_HOST = "https://kapi.kakao.com";
 
-    @Value("${kakao.redirect-uri}")
-    private String redirectUri;
-
-    @Value("${kakao.token-uri}")
-    private String tokenUri;
-
-    @Value("${kakao.user-info-uri}")
-    private String userInfoUri;
-
-    private final RestTemplate restTemplate;
-    private final UserService userService;
-    private final JWTUtil jwtUtil;
-
-    public String getAccessToken(String code) {
-        // 카카오 서버로부터 액세스 토큰을 요청하는 메서드
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(tokenUri)
-                .queryParam("grant_type", "authorization_code")
-                .queryParam("client_id", clientId)
-                .queryParam("redirect_uri", redirectUri)
-                .queryParam("code", code);
-
-        HttpEntity<?> request = new HttpEntity<>(headers);
-        ResponseEntity<Map> response = restTemplate.exchange(uriBuilder.toUriString(), HttpMethod.POST, request, Map.class);
-
-        if(response.getStatusCode() == HttpStatus.OK) {
-            Map<String, Object> responseBody = response.getBody();
-            return (String) responseBody.get("access_token");
-        }
-
-        throw new RuntimeException("Failed to get access token from Kakao");
-
+    @Autowired
+    public KakaoService(@Value("${kakao.client_id}") String clientId) {
+        this.clientId = clientId;
     }
 
-    public Map<String, Object> getUserInfo(String accessToken) {
-        // 액세스 토큰으로 사용자 정보를 요청하는 메서드
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-        HttpEntity<?> request = new HttpEntity<>(headers);
+    public String getAccessTokenFromKakao(String code) {
+        try {
+            KakaoTokenResponseDto kakaoTokenResponseDto = WebClient.create(KAUTH_TOKEN_URL_HOST).post()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/oauth/token")
+                            .queryParam("grant_type", "authorization_code")
+                            .queryParam("client_id", clientId)
+                            .queryParam("redirect_uri", "http://localhost:3000/user/kakao/callback")
+                            .queryParam("code", code)
+                            .build())
+                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                    .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+                        log.error("4xx error when requesting access token: {}", clientResponse.statusCode());
+                        return Mono.error(new RuntimeException("Invalid Parameter - " + clientResponse.statusCode()));
+                    })
+                    .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
+                        log.error("5xx error when requesting access token: {}", clientResponse.statusCode());
+                        return Mono.error(new RuntimeException("Internal Server Error - " + clientResponse.statusCode()));
+                    })
+                    .bodyToMono(KakaoTokenResponseDto.class)
+                    .block();
 
-        ResponseEntity<Map> response = restTemplate.exchange(userInfoUri, HttpMethod.GET, request, Map.class);
+            log.info(" [Kakao Service] Access Token ------> {}", kakaoTokenResponseDto.getAccessToken());
+            log.info(" [Kakao Service] Refresh Token ------> {}", kakaoTokenResponseDto.getRefreshToken());
+            log.info(" [Kakao Service] Id Token ------> {}", kakaoTokenResponseDto.getIdToken());
+            log.info(" [Kakao Service] Scope ------> {}", kakaoTokenResponseDto.getScope());
 
-        if(response.getStatusCode() == HttpStatus.OK) {
-            return response.getBody();
+            return kakaoTokenResponseDto.getAccessToken();
+        } catch (Exception e) {
+            log.error("Error occurred while getting access token from Kakao: ", e);
+            throw new RuntimeException("Failed to retrieve access token from Kakao", e);
         }
-
-        throw new RuntimeException("Failed to get user info from Kakao");
-
     }
-
-    public User processKakaoLogin(String code) {
-        // 카카오 로그인 처리 메서드
-        String accessToken = getAccessToken(code);
-        Map<String, Object> userInfo = getUserInfo(accessToken);
-
-        // 카카오에서 받아온 사용자 정보 추출
-        String email = (String) ((Map<String, Object>) userInfo.get("kakao_account")).get("email");
-        Long kakaoId = ((Number) userInfo.get("id")).longValue();
-        String nickname = (String) ((Map<String, Object>) userInfo.get("properties")).get("nickname");
-        String profileImg = (String) ((Map<String, Object>) userInfo.get("properties")).get("profile_image");
-
-        // DB에서 사용자 정보를 확인
-        Optional<User> optionalUser = userService.findByKakaoId(kakaoId);
-        User user;
-
-        if(optionalUser.isPresent()) {
-            // 사용자가 존재하면 그 사용자 정보를 가져옴
-            user = optionalUser.get();
-
-        }
-         else {
-            // 사용자가 없으면 회원가입 처리
-             user = User.builder()
-                     .kakaoId(kakaoId)
-                     .email(email)
-                     .nickname(nickname)
-                     .profileImg(profileImg)
-                     .build();
-        }
-
-         String accessTokenJwt = jwtUtil.createAccessToken(email);
-         String refreshTokenJwt = jwtUtil.createRefreshToken(email);
-
-        user.setRefreshToken(refreshTokenJwt);
-         userService.save(user);
-
-         return user;
-    }
-
 }
+
--- src/main/java/footlogger/footlog/web/controller/UserController.java
+++ src/main/java/footlogger/footlog/web/controller/UserController.java
@@ -5,8 +5,14 @@ import footlogger.footlog.service.KakaoService;
 import footlogger.footlog.service.UserService;
 import footlogger.footlog.web.dto.response.UserResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
@@ -15,9 +21,14 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/kakao/callback")
-    public UserResponseDto.LoginResultDto kakaoCallBack(@RequestParam String code) {
-        User user = kakaoService.processKakaoLogin(code);
-        return new UserResponseDto.LoginResultDto(user.getId(), user.getNickname(), user.getProfileImg());
+    public ResponseEntity<?> callback(@RequestParam("code") String code) throws IOException {
+        try {
+            String accessToken = kakaoService.getAccessTokenFromKakao(code);
+            return new ResponseEntity<>(accessToken, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Error during Kakao callback: ", e);
+            return new ResponseEntity<>("Failed to process Kakao callback", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/info")
