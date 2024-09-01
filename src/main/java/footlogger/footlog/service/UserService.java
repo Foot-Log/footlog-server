2e533293cd63e22eae2bcdf6eb32bc70ad2df81c
이예지 <oyatpeach@naver.com>
2024-09-01 14:29:47 +0900

    Feat : 회원 탈퇴 로직 구현 #25

--- src/main/java/footlogger/footlog/service/UserService.java
+++ src/main/java/footlogger/footlog/service/UserService.java
@@ -3,6 +3,7 @@ package footlogger.footlog.service;
 import footlogger.footlog.converter.UserConverter;
 import footlogger.footlog.domain.User;
 import footlogger.footlog.domain.jwt.JWTUtil;
+import footlogger.footlog.repository.LogRepository;
 import footlogger.footlog.repository.UserRepository;
 import footlogger.footlog.web.dto.response.UserResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final LogRepository logRepository;
 
     public Optional<User> findByKakaoId(Long kakaoId) {
         return userRepository.findByKakaoId(kakaoId);
@@ -29,6 +31,14 @@ public class UserService {
         User user = userRepository.findByAccessToken(token)
                 .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
         return UserConverter.toUserInfo(user);
+    }
 
+    @Transactional
+    public void deleteUser(String token) {
+        User user = userRepository.findByAccessToken(token)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+
+        logRepository.deleteByUser(user);
+        userRepository.delete(user);
     }
 }
