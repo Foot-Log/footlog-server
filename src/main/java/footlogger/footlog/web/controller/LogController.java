0f5a3d7d4f7547bddb8841b06fccb0f04ebeb6e7
이예지 <oyatpeach@naver.com>
2024-09-01 13:22:14 +0900

    Chore : ApiResponse 반환 #10

--- src/main/java/footlogger/footlog/web/controller/LogController.java
+++ src/main/java/footlogger/footlog/web/controller/LogController.java
@@ -1,6 +1,7 @@
 package footlogger.footlog.web.controller;
 
 import footlogger.footlog.domain.Log;
+import footlogger.footlog.payload.ApiResponse;
 import footlogger.footlog.service.LogService;
 import footlogger.footlog.web.dto.response.LogResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +14,15 @@ public class LogController {
     private final LogService logService;
 
     @GetMapping("/completedList")
-    public LogResponseDto.LogListDto getCompletedList(@RequestHeader("Authorization") String token){
+    public ApiResponse<LogResponseDto.LogListDto> getCompletedList(@RequestHeader("Authorization") String token){
         String tokenWithoutBearer = token.substring(7);
-        return logService.getCompletedList(tokenWithoutBearer);
+        LogResponseDto.LogListDto response = logService.getCompletedList(tokenWithoutBearer);
+        return ApiResponse.onSuccess(response);
    }
    @GetMapping("/detail/{logId}")
-    public LogResponseDto.LogDetailDto getLogDetail(@RequestHeader("Authorization") String token, @PathVariable("logId") Long logId){
+    public ApiResponse<LogResponseDto.LogDetailDto> getLogDetail(@RequestHeader("Authorization") String token, @PathVariable("logId") Long logId){
        String tokenWithoutBearer = token.substring(7);
-       return logService.getLogDetail(tokenWithoutBearer, logId);
+       LogResponseDto.LogDetailDto response = logService.getLogDetail(tokenWithoutBearer, logId);
+       return ApiResponse.onSuccess(response);
    }
 }
