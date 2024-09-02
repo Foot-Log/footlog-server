cc4806385cd328e09a8ecb9f9fc25f1754be6588
이예지 <oyatpeach@naver.com>
2024-09-02 18:25:58 +0900

    Feat : log update controller 구현 #10

--- src/main/java/footlogger/footlog/web/controller/LogController.java
+++ src/main/java/footlogger/footlog/web/controller/LogController.java
@@ -2,10 +2,19 @@ package footlogger.footlog.web.controller;
 
 import footlogger.footlog.domain.Log;
 import footlogger.footlog.payload.ApiResponse;
+import footlogger.footlog.payload.code.status.ErrorStatus;
 import footlogger.footlog.service.LogService;
 import footlogger.footlog.web.dto.response.LogResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+import static footlogger.footlog.payload.code.status.ErrorStatus._INTERNAL_SERVER_ERROR;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,8 +35,21 @@ public class LogController {
        return ApiResponse.onSuccess(response);
    }
 
-//   @PostMapping("/upload")
-//    public ApiResponse<> createLog(){
-//
-//   }
+   @PatchMapping(value = "update/{logId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse<LogResponseDto.LogDetailDto>> updateLog(
+           @RequestHeader("Authorization") String token,
+           @PathVariable("logId") Long logId,
+           @RequestParam("logContent") String logContent,
+           @RequestParam("existingUrls") List<String> existingUrls,
+           @RequestParam("newImages") List<MultipartFile> newImages
+           ) {
+        try{
+            String tokenWithoutBearer = token.substring(7);
+            LogResponseDto.LogDetailDto response = logService.updateLog(tokenWithoutBearer, logId, logContent,existingUrls, newImages);
+            return new ResponseEntity<>(ApiResponse.onSuccess(response), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(ApiResponse.onFailure(_INTERNAL_SERVER_ERROR, null), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+   }
 }
