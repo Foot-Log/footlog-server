0a4622cf37238478181d0d17633cd21be68a3f07
이예지 <oyatpeach@naver.com>
2024-09-06 19:18:59 +0900

    fix : webconfig 수정

--- src/main/java/footlogger/footlog/config/WebConfig.java
+++ src/main/java/footlogger/footlog/config/WebConfig.java
@@ -6,16 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private static final String CORS_URL_PATTERN = "/**";
-    private static final String CORS_METHOD = "*";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*") // 모든 HTTP 메서드 허용
-                .allowedHeaders("*") // 모든 헤더 허용
-                .exposedHeaders("Authorization"); // 필요한 경우 노출할 헤더
-                //.allowCredentials(true); // 쿠키나 인증 정보 사용 허용
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://localhost:8080/", "http://localhost:3000/")
+              .allowedMethods("GET", "POST", "PATCH", "DELETE")
+              .allowCredentials(true)
+              .maxAge(3000);
     }
 }
