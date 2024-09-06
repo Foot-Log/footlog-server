f9f78cb809e13856488b52b29fa082c1ed93832c
ity0526 <ity0526@gmail.com>
2024-09-06 17:10:24 +0900

    [Feat] 어노테이션 추가

--- src/main/java/footlogger/footlog/config/WebConfig.java
+++ src/main/java/footlogger/footlog/config/WebConfig.java
@@ -11,9 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping(CORS_URL_PATTERN)
+        registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods(CORS_METHOD) // 모든 HTTP 메서드 허용
+                .allowedMethods("*") // 모든 HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .exposedHeaders("Authorization"); // 필요한 경우 노출할 헤더
                 //.allowCredentials(true); // 쿠키나 인증 정보 사용 허용
