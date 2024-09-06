e850c3c8f1affc998314383388b2ea4b7fc6763f
이예지 <oyatpeach@naver.com>
2024-09-06 14:49:52 +0900

    chore : webConfig 수정

--- src/main/java/footlogger/footlog/config/WebConfig.java
+++ src/main/java/footlogger/footlog/config/WebConfig.java
@@ -12,10 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping(CORS_URL_PATTERN)
-                .allowedOrigins("http://localhost:3000", "http://43.201.255.209:8080")
+                .allowedOrigins("*")
                 .allowedMethods(CORS_METHOD) // 모든 HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
-                .exposedHeaders("Authorization") // 필요한 경우 노출할 헤더
-                .allowCredentials(true); // 쿠키나 인증 정보 사용 허용
+                .exposedHeaders("Authorization"); // 필요한 경우 노출할 헤더
+                //.allowCredentials(true); // 쿠키나 인증 정보 사용 허용
     }
 }
