572459239bc360367ec9e9315c178f743725811a
ity0526 <ity0526@gmail.com>
2024-09-11 17:51:24 +0900

    [Feat] PreferenceRequestBody에 Getter 어노테이션 추가

--- src/main/java/footlogger/footlog/web/dto/response/PreferenceRequestBody.java
+++ src/main/java/footlogger/footlog/web/dto/response/PreferenceRequestBody.java
@@ -1,8 +1,10 @@
 package footlogger.footlog.web.dto.response;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class PreferenceRequestBody {
     private String firstKeyword;
     private String secondKeyword;
