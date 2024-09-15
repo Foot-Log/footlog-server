25d46b419f83cb8de685f8ac69838b98ff78264c
이예지 <oyatpeach@naver.com>
2024-09-15 22:40:10 +0900

    Feat : 완주한 리스트 dto 로직 변경 #10

--- src/main/java/footlogger/footlog/converter/LogConverter.java
+++ src/main/java/footlogger/footlog/converter/LogConverter.java
@@ -5,19 +5,20 @@ import footlogger.footlog.domain.LogPhoto;
 import footlogger.footlog.domain.User;
 import footlogger.footlog.web.dto.response.LogResponseDto;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogConverter {
-    public static LogResponseDto.LogListDto toLogList(User user) {
-        return LogResponseDto.LogListDto.builder()
-                .logList(user.getLogList().stream()
-                        .map(LogConverter::toLogName).collect(Collectors.toList()))
-                .build();
+    public static List<LogResponseDto.LogDto> toLogList(User user) {
+        return user.getLogList().stream()
+                .map(LogConverter::toLog)
+                .collect(Collectors.toList());
     }
-    public static LogResponseDto.LogNameDto toLogName(Log log){
-        return LogResponseDto.LogNameDto.builder()
+    public static LogResponseDto.LogDto toLog(Log log){
+        return LogResponseDto.LogDto.builder()
                 .logId(log.getId())
                 .address(log.getCourse().getAddress())
+                .name(log.getCourse().getName())
                 .build();
     }
     public static LogResponseDto.LogDetailDto toLogDetail(Log log) {
