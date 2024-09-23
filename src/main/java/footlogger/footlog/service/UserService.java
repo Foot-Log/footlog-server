2548982c84d7e7aacc743c9681b997bf4f85390f
ity0526 <ity0526@gmail.com>
2024-09-23 10:44:40 +0900

    [Feat] 유저 삭제 시 Redis 서버 데이터 삭제

--- src/main/java/footlogger/footlog/service/UserService.java
+++ src/main/java/footlogger/footlog/service/UserService.java
@@ -7,6 +7,7 @@ import footlogger.footlog.repository.LogRepository;
 import footlogger.footlog.repository.UserRepository;
 import footlogger.footlog.web.dto.response.UserResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,8 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
     private final LogRepository logRepository;
+    private final RedisTemplate<String, Long> courseRedisTemplate;
+    private final RedisTemplate<String, String> searchRedisTemplate;
 
     public User findByKakaoId(Long kakaoId) {
         return userRepository.findByKakaoId(kakaoId).orElse(null);
@@ -37,6 +40,10 @@ public class UserService {
     public void deleteUser(String token) {
         User user = userRepository.findByAccessToken(token)
                 .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+
+        //redis 서버에 저장된 정보 삭제
+        courseRedisTemplate.delete("Course" + user.getId());
+        searchRedisTemplate.delete("SearchLog" + user.getId());
 
         logRepository.deleteByUser(user);
         userRepository.delete(user);
